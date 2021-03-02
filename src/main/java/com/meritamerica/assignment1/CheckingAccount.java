6e136304997b34d999ae611abfa5cd950f745d7c
Tian B <60415419+tsboothe@users.noreply.github.com>
2021-03-01 18:25:51 -0600

    Update CheckingAccount.java

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -1,5 +1,79 @@
 package com.meritamerica.assignment1;
 
 public class CheckingAccount {
+	private static final String OpeningBalance = null;
+	private CheckingAccount openingBalance;
+	private double getBalance;
+	private double getInterestRate;
+	private boolean withdraw;
+	private boolean deposit;
+	private double futureValue;
 	
+	public double OpeningBalance() {
+		return futureValue;}
+	
+	CheckingAccount(
+			
+			double openingBalance,
+			double getBalance, 
+			double getInterestRate,
+			boolean withdraw,
+			boolean deposit,
+			double futureValue
+			
+			) {
+		this.getBalance = getBalance;
+		this.getInterestRate = getInterestRate;
+		this.withdraw =withdraw;
+		this.deposit = deposit;
+		this.futureValue = futureValue;
+		openingBalance = new openingBalance(OpeningBalance);
+
+	}
+
+	
+	public double getBalance() {
+		return getBalance;
+	}
+	
+	public void deposit(double amount) {
+		
+		if (amount<=0) {
+			System.out.println("Amount entered is invalid");
+		}
+		else
+		{
+			getBalance = getBalance + amount;
+			openingBalance.deposit(amount) ;
+			return;
+		}
+	}
+	
+	public void withdraw(double amount) {
+		
+		if (amount<=0) {
+			System.out.println("Amount entered is invalid");
+		}
+		else 
+		{
+			if (getBalance < amount) {
+				System.out.println("Insufficient funds");
+			}
+				else {
+					getBalance = getBalance - amount;
+				}
+		}
+		openingBalance.withdraw(amount);
+		return;
+		}
+	
+	public void futureValue(int years, int presentValue) {
+			
+			if (years != 1) {
+				return (presentValue * ((presentValue * 0.01), years - 1));
+			}
+			else {
+			return presentValue;
+		}
+	}
 }
